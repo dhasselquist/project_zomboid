2a86868f926d59873bedbb101726fe61773bb5d9
Dylan <dhasselquist@gmail.com>
2017-02-01 14:13:42 -0800

    Initial commit for graphical recipe functionality.

--- controllers/index.js
+++ controllers/index.js
@@ -3,18 +3,48 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
 var Weapon = require('./weapon_model.js');
+var Recipe = require('./recipe_model.js');
 module.exports.controller = function(app){
-    //Weapon Comparison logic
-    app.get('/getWeapons', function(req,res){
-	mongoose.connect('mongodb://localhost/projectZomboid');
-        var db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', function() {
-            console.log('connection successful');
-	    Weapon.find(function(err, docs){
-			mongoose.connection.close();
-			res.json(docs);
-	    });
-        });
-    });
+	//Weapon Comparison logic
+	app.get('/getWeapons', function(req,res){
+		mongoose.connect('mongodb://localhost/projectZomboid');
+		var db = mongoose.connection;
+		db.on('error', console.error.bind(console, 'connection error:'));
+		db.once('open', function() {
+			console.log('connection successful');
+			Weapon.find(function(err, docs){
+				res.json(docs);
+				mongoose.connection.close();
+			});
+		});
+	});
+	app.post('/getIngredients', function(req,res){
+		mongoose.connect('mongodb://localhost/projectZomboid');
+		var db = mongoose.connection;
+		db.on('error', console.error.bind(console, 'Connection error: '));
+		db.once('open', function() {
+			console.log('Getting Ingredients for:' + req.body.params.recipe);
+			Recipe.find({recipe:req.body.params.recipe}, function(err, docs){
+				if (err) console.log(err);
+				res.json(docs);
+				mongoose.connection.close();
+			});
+			//mongoose.connection.close();
+		});
+	});
+	app.get('/getRecipes', function(req,res){
+		mongoose.connect('mongodb://localhost/projectZomboid');
+		var db = mongoose.connection;
+		db.on('error', console.error.bind(console, 'connection error:'));
+		db.once('open', function() {
+			console.log('Connection established: Get Recipes.');
+			//Recipe.find({}, 'recipe', function(err, docs){
+			Recipe.distinct('recipe', function(err, docs){
+				mongoose.connection.close();
+				res.json(docs);
+			});
+		});
+
+	});
+
 }
--- server.js
+++ server.js
@@ -5,8 +5,6 @@
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-    var route = require('./controllers/index.js');
-    route.controller(app);
 
     // configuration =================
     app.set('view engine', 'ejs');
@@ -18,6 +16,8 @@
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
+    var route = require('./controllers/index.js');
+    route.controller(app);
     // listen (start app with node server.js) ======================================
     app.listen(8081);
     console.log("App listening on port 8081");
