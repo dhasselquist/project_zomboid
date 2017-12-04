fe76eac7da3c63fa3acbf0c1f9ceccdde8fd15c0
Dylan Laptop <dhasselquist@gmail.com>
2017-12-04 09:33:04 -0800

    refactoring item checklist functions

--- content/weaponCompare.js
+++ content/weaponCompare.js
@@ -1,95 +1,72 @@
 angular.module('zomboid', [])
-	.controller('main', ['$scope', '$http', function($scope, $http) {
+.controller('main', ['$scope', '$http', function($scope, $http) {
 		$scope.data = {}, $scope.showTable = false, $scope.metricResults = [], $scope.weapons = [];
 		$scope.advantagesFirst = function(card) {
-			if (card.result == "Advantage") return 0;
-			else if (card.result == "Equal") return 1;
-			else return 2;
+		if (card.result == "Advantage") return 0;
+		else if (card.result == "Equal") return 1;
+		else return 2;
 		}
 		$scope.itemChecklist = [];
 		$scope.newItem = '';
 		$scope.addTools = function() {
-			$scope.newGroup = {};
-			$scope.newGroup.name = 'Tools';
-			$scope.newGroup.items = [
-				{item:'Hammer'},
-				{item:'Screwdriver'},
-				{item:'Saw'},
-				{item:'Crowbar'},
-				{item:'Sledgehammer'}
-			];
-			$scope.itemChecklist.push($scope.newGroup);
-
-
+		$scope.newGroup = {};
+		$scope.newGroup.name = 'Tools';
+		$scope.newGroup.items = [
+		{item:'Hammer'},
+		{item:'Screwdriver'},
+		{item:'Saw'},
+		{item:'Crowbar'},
+		{item:'Sledgehammer'}
+		];
+		$scope.itemChecklist.push($scope.newGroup);
 		}
 		$scope.addBooks = function() {
 			$scope.newGroup = {};
 			$scope.newGroup.name = 'Books';
 			$scope.newGroup.items = [
 			{item:'Beginner Carpentry'},
-				{item:'Intermediate Carpentry'},
-				{item:'Advanced Carpentry'},
-				{item:'Expert Carpentry'},
-				{item:'Master Carpentry'},
+			{item:'Intermediate Carpentry'},
+			{item:'Advanced Carpentry'},
+			{item:'Expert Carpentry'},
+			{item:'Master Carpentry'},
 
-				{item:'Beginner Cooking'},
-				{item:'Intermediate Cooking'},
-				{item:'Advanced Cooking'},
-				{item:'Expert Cooking'},
-				{item:'Master Cooking'},
+			{item:'Beginner Cooking'},
+			{item:'Intermediate Cooking'},
+			{item:'Advanced Cooking'},
+			{item:'Expert Cooking'},
+			{item:'Master Cooking'},
 
-				{item:'Beginner Farming'},
-				{item:'Intermediate Farming'},
-				{item:'Advanced Farming'},
-				{item:'Expert Farming'},
-				{item:'Master Farming'},
+			{item:'Beginner Farming'},
+			{item:'Intermediate Farming'},
+			{item:'Advanced Farming'},
+			{item:'Expert Farming'},
+			{item:'Master Farming'},
 
-				{item:'Beginner First Aid'},
-				{item:'Intermediate First Aid'},
-				{item:'Advanced First Aid'},
-				{item:'Expert First Aid'},
-				{item:'Master First Aid'},
+			{item:'Beginner First Aid'},
+			{item:'Intermediate First Aid'},
+			{item:'Advanced First Aid'},
+			{item:'Expert First Aid'},
+			{item:'Master First Aid'},
 
-				{item:'Beginner Electrical'},
-				{item:'Intermediate Electrical'},
-				{item:'Advanced Electrical'},
-				{item:'Expert Electrical'},
-				{item:'Master Electrical'}
+			{item:'Beginner Electrical'},
+			{item:'Intermediate Electrical'},
+			{item:'Advanced Electrical'},
+			{item:'Expert Electrical'},
+			{item:'Master Electrical'}
 			];
 			$scope.itemChecklist.push($scope.newGroup);
 		}
-		$scope.removeGroup = function(group){
-			$scope.itemChecklist.splice(group,1);
-		}
-		$scope.groupEnter = function(keyCode,group,groupName){
-			console.log(keyCode,group)
-			if (keyCode == 13){
-				$scope.itemChecklist[group].items.push({item:''});
-			var elementFocus = '#' + groupName + ' input';
-			setTimeout(function(){
-				$(elementFocus).last().focus();
-			},0);
-			}
-	
-		}
 		$scope.addItemEnter = function(group,groupName,keyCode){
-			if (keyCode==13){
-			$scope.itemChecklist[group].items.push({item:''});
-			var elementFocus = '#' + groupName + ' input';
-			setTimeout(function(){
-				$(elementFocus).last().focus();
-			},0);
+			//User hit enter or keyCode is undefined
+			if (keyCode == 13 || keyCode == undefined){
+				$scope.itemChecklist[group].items.push({item:''});
+				var elementFocus = '#' + groupName + ' input';
+				setTimeout(function(){
+						$(elementFocus).last().focus();
+						},0);
 			}
-		}
-		$scope.addItem = function(group,groupName) {
-			$scope.itemChecklist[group].items.push({item:''});
-			var elementFocus = '#' + groupName + ' input';
-			setTimeout(function(){
-				$(elementFocus).last().focus();
-			},0);
 		}
 		$scope.addGroup = function() {
-			console.log('test');
 			$scope.newGroup = {};
 			$scope.newGroup.name = 'New Group';
 			$scope.newGroup.items = [];
@@ -98,6 +75,9 @@ angular.module('zomboid', [])
 		}
 		$scope.listGroups = function() { 
 			console.log($scope.itemChecklist);
+		}
+		$scope.removeGroup = function(group){
+			$scope.itemChecklist.splice(group,1);
 		}
 		$scope.remove = function(item,group){
 			$scope.itemChecklist[group].items.splice(item,1);
@@ -108,8 +88,8 @@ angular.module('zomboid', [])
 		}
 		$http.get('/getWeapons')
 			.then(function(response) {
-				$scope.weapons = response.data;
-			});
+					$scope.weapons = response.data;
+					});
 		$scope.checkInputs = function() {
 			if ($scope.data.weaponOne != undefined && $scope.data.weaponTwo != undefined) {
 				$scope.weaponAdvantages();
@@ -119,36 +99,36 @@ angular.module('zomboid', [])
 		$scope.weaponAdvantages = function() {
 			$scope.metrics = [{
 				"metric": "MaxDamage",
-				"displayName": "Maximum Damage",
-				"advantage": ">"
+					"displayName": "Maximum Damage",
+					"advantage": ">"
 			}, {
 				"metric": "MinDamage",
-				"displayName": "Minimum Damage",
-				"advantage": ">"
+					"displayName": "Minimum Damage",
+					"advantage": ">"
 			}, {
 				"metric": "CriticalChance",
-				"displayName": "Critical Chance",
-				"advantage": ">"
+					"displayName": "Critical Chance",
+					"advantage": ">"
 			}, {
 				"metric": "Weight",
-				"displayName": "Weight",
-				"advantage": "<"
+					"displayName": "Weight",
+					"advantage": "<"
 			}, {
 				"metric": "ConditionMax",
-				"displayName": "Maximum Durability",
-				"advantage": ">"
+					"displayName": "Maximum Durability",
+					"advantage": ">"
 			}, {
 				"metric": "ConditionLowerChanceOneIn",
-				"displayName": "Odds of Breaking (lower is more often)",
-				"advantage": ">"
+					"displayName": "Odds of Breaking (lower is more often)",
+					"advantage": ">"
 			}, {
 				"metric": "MaxRange",
-				"displayName": "Maximum Range",
-				"advantage": ">"
+					"displayName": "Maximum Range",
+					"advantage": ">"
 			}, {
 				"metric": "SwingTime",
-				"displayName": "Weapon Speed",
-				"advantage": "<"
+					"displayName": "Weapon Speed",
+					"advantage": "<"
 			}];
 			$scope.metricResults = [];
 			var a = $scope.data.weaponOne;
@@ -172,5 +152,5 @@ angular.module('zomboid', [])
 			}
 			return;
 		}
-	}]);
+}]);
 
